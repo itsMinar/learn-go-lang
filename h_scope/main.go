84362612bf3d51ef23533a462f4d902fbc61a4aa
Md. Minar Munshi <minarmunshi46@gmail.com>
2025-08-28 20:15:17 +0600

    28-Aug-2025

--- h_scope/main.go
+++ h_scope/main.go
@@ -1,15 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"h_scope/mathlib"
+)
 
 var a = 20
 var b = 30
 
-func add(x int, y int) {
-	z := x + y
-	fmt.Println(z)
-}
-
 func main() {
 	fmt.Println("Welcome to Scope in Go Lang")
+
+	p := 30
+	q := 40
+
+	mathlib.Add(p, q)
+
+	mathlib.Add(a, b)
+
+	mathlib.Add(a, p)
+
 }
