bdf00681161c18decae2a1f3b8ab3116c3716952
Md. Minar Munshi <minarmunshi46@gmail.com>
2025-09-30 18:47:25 +0600

    30-Sep-2025

--- h_pointers/main.go
+++ h_pointers/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type User struct {
+	Name     string
+	Age      int
+	Salary   float64
+	FavFoods []string
+}
+
+func print(numbers *[3]int) {
+	fmt.Println(numbers)
+}
+
+func printUserInfo(user *User) {
+	fmt.Println("User Name:", user.Name)
+	fmt.Println("User Age:", user.Age)
+	fmt.Println("User Salary:", user.Salary)
+	fmt.Println("User FavFoods:", user.FavFoods)
+}
 
 func main() {
 	fmt.Println("Welcome to Pointers in Go lang")
+
+	// x := 20
+
+	// p := &x // address of x
+	// v := *p // value at address p
+
+	// fmt.Println("Address of x is:", p)
+	// fmt.Println("Value of x using pointer is:", v)
+
+	arr := [3]int{1, 2, 3}
+	print(&arr)
+
+	user := User{
+		Name:     "Minar",
+		Age:      25,
+		Salary:   5000,
+		FavFoods: []string{"Pizza", "Burger"},
+	}
+
+	printUserInfo(&user)
 }
